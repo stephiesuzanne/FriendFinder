bade72734bee4aea8bdde1e017614739f6f8b18d
Stephanie Davis <stephiesuzanne@outlook.com>
2020-01-03 11:35:42 +0800

    correct routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,10 +2,10 @@ var path = require("path");
 
 module.exports = function(app) {
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname + "/../public/survey.html"));
+    res.sendFile(path.join(__dirname + "FriendFinder/app/public/css/survey.html"));
   });
 
   app.use(function(req, res) {
-    res.sendFile(path.join(__dirname + "/../public/home.html"));
+    res.sendFile(path.join(__dirname + "FriendFinder/app/public/css/home.html"));
   });
 };
