88128806fb54763fb45df01a51da98bcc5f12df2
Stephanie Davis <stephiesuzanne@outlook.com>
2020-01-03 11:05:11 +0800

    made friends list

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+var friends = [
+    {
+      name: "Selena Gomez",
+      photo:
+        "https://www.cheatsheet.com/wp-content/uploads/2020/01/selena-gomez-amas-2019-1024x683.jpg",
+      scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+      name: "James McAvoy",
+      photo:
+        "https://m.media-amazon.com/images/M/MV5BMTQzNzIxOTYzMl5BMl5BanBnXkFtZTcwNjYxNTk1Nw@@._V1_.jpg",
+      scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
+      name: "Jennifer Lopez",
+      photo:
+        "https://www.cheatsheet.com/wp-content/uploads/2020/01/Jennifer-Lopez.jpg",
+      scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    },
+    {
+      name: "Henry Cavill",
+      photo:
+        "https://m.media-amazon.com/images/M/MV5BMTUxNTExMzUzOF5BMl5BanBnXkFtZTgwOTI1MjA3OTE@._V1_.jpg",
+      scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    },
+    {
+      name: "Emma Stone",
+      photo:
+        "https://www.goldenglobes.com/sites/default/files/styles/portrait_medium/public/people/cover_images/emma_stone_091316_la_la_land_4c.jpg?itok=45XyUZRd",
+      scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    },
+    {
+      name: "Blake Lively",
+      photo:
+        "https://media.vanityfair.com/photos/560c07690404673e0de66e1d/master/w_960,c_limit/blake-lively-end-of-preserve-01.jpg",
+      scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+    },
+    {
+      name: "Luke Evans",
+      photo:
+        "https://www.out.com/sites/out.com/files/lukeevans-750x.jpg",
+      scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    }
+  ];
+  module.exports = friends;
